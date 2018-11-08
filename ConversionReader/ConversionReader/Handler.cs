a4c46158b0c85561ca739062bb791cf8d0683614
linkenski <linkenski@gmail.com>
2018-11-08 13:28:02 +0100

    Handler class dummy
    The database handler class
    Methods: GetLineCode() and GetTermCode

---
+++ ConversionReader/ConversionReader/Handler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+
+namespace ConversionReader
+{
+    class Handler
+    {
+        //Fields: SQL Data object which references the ConversionReader table
+        public Handler() { }
+        public string GetLineCode()
+        {
+            return "placeholder";
+        }
+        public string GetPier()
+        {
+            return "placeholer";
+        }
+    }
+}
