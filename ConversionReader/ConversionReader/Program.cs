7215d35450275ee4dee0f146058eb18030c5ce65
linkenski <linkenski@gmail.com>
2018-11-08 11:53:42 +0100

    Base project files
    Empty console project with form

---
+++ ConversionReader/ConversionReader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversionReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainWindow form1 = new MainWindow();
+
+            Application.Run(form1);
+        }
+    }
+}
