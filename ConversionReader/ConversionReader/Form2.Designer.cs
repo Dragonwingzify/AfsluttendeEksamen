088e4e71ccde76f95438ce376fce3b4ebe28ea04
linkenski <linkenski@gmail.com>
2018-12-03 10:13:32 +0100

    Update Form2.Designer.cs
    Uspecificerede småting i form2.

--- ConversionReader/ConversionReader/Form2.Designer.cs
+++ ConversionReader/ConversionReader/Form2.Designer.cs
@@ -31,12 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,8 +64,8 @@
             // 
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Location = new System.Drawing.Point(102, 43);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 222);
@@ -72,41 +73,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Navigation";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(6, 33);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(188, 32);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Write Conversion";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(6, 71);
+            this.button3.Location = new System.Drawing.Point(6, 41);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(188, 39);
+            this.button3.Size = new System.Drawing.Size(188, 37);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Add Row";
+            this.button3.Text = "Write Conversion";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(6, 116);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(188, 38);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Delete Row";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(6, 160);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(188, 34);
-            this.button5.TabIndex = 3;
-            this.button5.Text = "Edit Row";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
@@ -125,6 +99,33 @@
             this.button7.TabIndex = 4;
             this.button7.Text = "Go To Table";
             this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add Row";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(188, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Delete Row";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 170);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(188, 37);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Edit Row";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
@@ -148,11 +149,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
     }
 }
