be4c666e327187bf35c0526314766cfa39c7dcff
Dennis <denn3744@eadania.student.dk>
2018-12-20 13:05:46 +0100

    Assembly name not needed
    Removed Culture under assembly

--- ConversionReader/ConversionReader/Properties/AssemblyInfo.cs
+++ ConversionReader/ConversionReader/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ConversionReader")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("Dania")]
-[assembly: AssemblyCulture("Culture")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
